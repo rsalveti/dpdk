d64e36931755e901492cb6a833bdcb1fd7fb0b84
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2016-01-18 15:01:03 +0100

    Move the SDK Env vars out of profile, but keep them in /usr/share/dpdk Update readme about that and make it belong to dpdk-doc. Fix the exported RTE_TARGET directory

--- debian/dpdk-sdk-env.sh
+++ debian/dpdk-sdk-env.sh
@@ -1,5 +1,3 @@
-if [ -z "${RTE_SDK}" ]; then
-    export RTE_TARGET="$(shell uname -m)-native-linuxapp-gcc"
-    export RTE_SDK="/usr/share/dpdk/"
-    export RTE_INCLUDE="/usr/include/dpdk"
-fi
+export RTE_TARGET="$(uname -m)-default-linuxapp-gcc"
+export RTE_SDK="/usr/share/dpdk/"
+export RTE_INCLUDE="/usr/include/dpdk"
